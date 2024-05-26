9f96408cc59a669d71d245288ea96e64bd8e87f4
gmoro20 <145460535+gmoro20@users.noreply.github.com>
2024-05-26 22:08:56 +0200

    UPDATE 3

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -5,16 +5,5 @@ function resetForm() {
 
 // Define la función send
 function send() {
-	try {
-	    const data = {
-	        "instalacion": document.getElementById('instalacion').value,
-		"deporte": document.getElementById('deporte').value,
-		"campo": document.getElementById('campo').value,
-		"fecha": document.getElementById('fecha').value		
-	    	};
-		Telegram.WebApp.sendData(JSON.stringify(data));
-	} catch (error) {
-	    // Captura cualquier excepción que ocurra y maneja el error
-	    Telegram.WebApp.sendData("Se ha producido un error:", error);
-	}
+	Telegram.WebApp.sendData("hola");
 };
