7edd12d6d6bad59aa76fa66cc6b4989d3fd54de3
gmoro20 <145460535+gmoro20@users.noreply.github.com>
2024-05-26 21:51:53 +0200

    UPDATE

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -7,10 +7,9 @@ function resetForm() {
 function send() {
 	const data = {
         "instalacion": document.getElementById('instalacion').value,
-		"deporte": document.getElementById('deporte').value,
-		"campo": document.getElementById('campo').value,
-		"fecha": document.getElementById('fecha').value		
-    };
-	const jsonData = JSON.stringify(data);
-    Telegram.WebApp.sendData(JSON.stringify(data))
+	"deporte": document.getElementById('deporte').value,
+	"campo": document.getElementById('campo').value,
+	"fecha": document.getElementById('fecha').value		
+    	};
+	Telegram.WebApp.sendData(JSON.stringify(data));
 }
