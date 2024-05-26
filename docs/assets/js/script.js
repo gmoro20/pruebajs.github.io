a641bc65da632e23e8ff06b22ba0a01f45dce7b5
Gorka Moro <gorkamoro22@gmail.com>
2024-05-26 21:12:15 +0200

    UPDATE: Prueba 1

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,3 +1,14 @@
-document.getElementById('cancelarBtn').addEventListener('click', function() {
+// Define la función delete
+function delete() {
     document.getElementById('reservaForm').reset();
-});
+}
+// Define la función send
+function send() {
+	const data = {
+        "instalacion": document.getElementById('instalacion').value,
+		"deporte": document.getElementById('deporte').value,
+		"campo": document.getElementById('campo').value,
+		"fecha": document.getElementById('fecha').value		
+    }
+    Telegram.WebApp.sendData(data)
+}
